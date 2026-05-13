5abdfb04ca13b55759c473826708f712c3e1b7f1
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-13 10:53:50 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Map } from '@vis.gl/react-google-maps';
 
 function App() {
   
@@ -7,6 +8,13 @@ function App() {
   return (
     <>
       <h1>react google map</h1>
+ <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 23.8103, lng: 90.4125}}
+      defaultZoom={10}
+      gestureHandling='greedy'
+      disableDefaultUI
+    />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { googleMapAPIKEY } from './map-api.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={googleMapAPIKEY}>
+      <App />
+    </APIProvider>
   </StrictMode>,
 )
